dc8cbf75e3ec473ed8ef25b893e17b225e47fdc2
KanizNUBTK <kaniz.m.mim@gmail.com>
2022-03-02 12:41:49 +0600

    login/sign uo form

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Link href="/text">Test</Link>
+        <Link href="./Login/login.js">login</Link>
+      </div>
+    )
+  }
