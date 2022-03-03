465c140c75cc69e3374bc78a1faac29d1fe212bb
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-03-03 13:05:43 +0600

    done

--- pages/login.js
+++ pages/login.js
@@ -74,7 +74,7 @@ const App = () => {
 							<Button
 								type="primary"
 								htmlType="submit"
-								className="btn btn-blue"
+								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 							>
 								Log in
 							</Button>
