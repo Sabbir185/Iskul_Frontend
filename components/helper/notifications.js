b438d187cbb699ab6c32cb12f6dc8adeea3fdfe4
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-03-08 00:44:07 +0600

    profile implemented

---
+++ components/helper/notifications.js
@@ -0,0 +1,11 @@
+import { FaSnapchatGhost } from "react-icons/fa";
+
+const Notifications = () => {
+	return (
+		<div className="h-3 mr-8 cursor-pointer" style={{color: 'goldenrod', fontSize: '23px'}}>
+			<FaSnapchatGhost />
+		</div>
+	);
+};
+
+export default Notifications;
--- components/navbar/index.js
+++ components/navbar/index.js
@@ -2,19 +2,25 @@ import Link from "next/link";
 import { AiOutlineAlignLeft } from "react-icons/ai";
 import classes from "./navbar.module.css";
 import InputForm from "../helper/inputForm";
+import Profile from "../userProfile";
+import Notifications from "../helper/notifications";
 
-const Navbar = ({toggleChange}) => {
+const Navbar = ({ toggleChange }) => {
 	return (
 		<section className="flex flex-row items-center justify-between">
 			<div className="flex pt-4 pl-10">
 				<div className="flex items-center pr-5">
-					<AiOutlineAlignLeft className={classes.icon1} onClick={toggleChange}/>
+					<AiOutlineAlignLeft
+						className={classes.icon1}
+						onClick={toggleChange}
+					/>
 				</div>
 				<InputForm />
 			</div>
 
-			<div>
-				<h1>Profile</h1>
+			<div className="flex items-center">
+				<Notifications />
+				<Profile />
 			</div>
 		</section>
 	);
