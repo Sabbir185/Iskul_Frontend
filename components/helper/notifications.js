10a64bf1166aa82c99a804c27096dc3eb047b480
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-03-08 10:41:12 +0600

    style updated

--- components/helper/notifications.js
+++ components/helper/notifications.js
@@ -1,11 +1,13 @@
-import { FaSnapchatGhost } from "react-icons/fa";
-
+import { MdNotificationImportant } from "react-icons/md";
 const Notifications = () => {
-	return (
-		<div className="h-3 mr-8 cursor-pointer" style={{color: 'goldenrod', fontSize: '23px'}}>
-			<FaSnapchatGhost />
-		</div>
-	);
+  return (
+    <div
+      className="h-3 mr-8 cursor-pointer"
+      style={{ color: "goldenrod", fontSize: "23px" }}
+    >
+      <MdNotificationImportant />
+    </div>
+  );
 };
 
 export default Notifications;
