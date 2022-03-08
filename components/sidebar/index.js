a9456208082f31668728acfaf51602a5a24d6d1e
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-03-08 10:04:26 +0600

    style updated

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,25 +1,24 @@
-import React from "react";
-import logo from "../../public/images/logo.png";
-import logo2 from "../../public/images/logo2.png";
 import Image from "next/image";
+import React from "react";
+import logo2 from "../../public/images/logo2.png";
 
 const SideMenu = () => {
-	return (
-		<section className="flex flex-col items-center justify-center">
-			<div className="w-56 flex flex-col items-center justify-center rounded-full relative border-b-2">
-				<Image className="shrink-0 " src={logo2} alt="logo" />
-				<p className="mt-0 pt-0 absolute bottom-6 text-lg font-bold text-gray-600 odd:">
-					Skul Management
-				</p>
-			</div>
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <div className="w-56 flex flex-col items-center justify-center rounded-full relative border-b-2">
+        <Image className="shrink-0 " src={logo2} alt="logo" />
+        <p className="mt-0 pt-0 absolute bottom-6 text-lg font-bold text-gray-600 odd:">
+          Iskul Management
+        </p>
+      </div>
 
-			<div className="mt-5">
-				<h1>Dashboard</h1>
-				<h1>Management</h1>
-				<h1>Other</h1>
-			</div>
-		</section>
-	);
+      <div className="mt-5">
+        <h1>Dashboard</h1>
+        <h1>Management</h1>
+        <h1>Others</h1>
+      </div>
+    </section>
+  );
 };
 
 export default SideMenu;
