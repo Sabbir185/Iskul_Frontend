d36e93b2e46c04a531a84dbad66089ce9ee2d0db
KanizNUBTK <kaniz.m.mim@gmail.com>
2022-03-09 16:06:24 +0600

    sign and Login form

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import 'antd/dist/antd.css';
 import "metismenujs/dist/metismenujs.css";
+import AppContext from "../context/AppContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AppContext.Provider>
       <Component {...pageProps} />;
-    </>
+    </AppContext.Provider>
   );
 }
 
