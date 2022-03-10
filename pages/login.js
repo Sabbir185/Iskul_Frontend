befdbf59b980157d706fbf6f9aaa2820665d6814
KanizNUBTK <kaniz.m.mim@gmail.com>
2022-03-10 11:45:55 +0600

    sign up

--- pages/login.js
+++ pages/login.js
@@ -29,7 +29,7 @@ const Login = () => {
                         <h2>in</h2>
                     </span>
                 </div>
-                <div className="flex items-center justify-center space-x-2">
+                <div className="flex items-center justify-center space-x-2 mt-2">
                     <span className="h-px w-16 bg-gray-300"></span>
                     <span className="text-gray-500 font-normal">OR</span>
                     <span className="h-px w-16 bg-gray-300"></span>
--- pages/signup.js
+++ pages/signup.js
@@ -9,10 +9,10 @@ const Signup = () => {
 
 	return (
 	<div>
-        <div className="relative md:h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center bg-img">
+        <div className="relative md:h-screen flex items-center justify-center bg-blend-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-300 bg-no-repeat bg-cover relative items-center bg-img">
             <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
-            <div className="flex flex-wrap w-3/5 space-y-4  place-content-between py-6 px-10 bg-white rounded-xl z-10">
-                <div className="w-2/5 my-auto">
+            <div className="md:flex md:flex-wrap md:w-3/5 md:space-y-4 md:place-content-between py-6 px-10 bg-white rounded-xl z-10">
+                <div className="md:w-2/5 md:my-auto">
                     <div className="text-center">
                         <h2 className="mt-1 text-3xl font-bold text-gray-900">
                             Welcom To Skul Management
@@ -23,8 +23,15 @@ const Signup = () => {
                         <span className="w-11 h-11 items-center justify-center inline-flex rounded-full font-bold text-lg  text-white  bg-blue-900 hover:shadow-lg cursor-pointer transition ease-in duration-300"><h2>fb</h2></span><span className="w-11 h-11 items-center justify-center inline-flex rounded-full font-bold text-lg  text-white  bg-blue-300 hover:shadow-lg cursor-pointer transition ease-in duration-300"> <h2>tw</h2></span>
                         <span className="w-11 h-11 items-center justify-center inline-flex rounded-full font-bold text-lg  text-white bg-blue-500 hover:shadow-lg cursor-pointer transition ease-in duration-300"><h2>in</h2></span>
                     </div>
+                    <div className=" md:hidden mt-2">
+                        <div className="flex items-center justify-center space-x-2 ">
+                        <span className="h-px w-16 bg-gray-300"></span>
+                        <span className="text-gray-500 font-normal">OR</span>
+                        <span className="h-px w-16 bg-gray-300"></span>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-3/5 border-l-2 p-10">
+                <div className="md:w-3/5 md:border-l-2 md:p-10">
                     <Form
                         onFinish={onFinish}
                         initialValues={{
@@ -45,7 +52,7 @@ const Signup = () => {
                                 </label>
                             }
                         >
-                            <input className="shadow appearance-none border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+                            <input className="shadow appearance-none border w-full py-2 px-3 text-gray-700 md:mb-3 leading-tight focus:outline-none focus:shadow-outline" />
                         </Form.Item>
                         <Form.Item
                             name="lastName"
@@ -61,7 +68,7 @@ const Signup = () => {
                                 </label>
                             }
                         >
-                            <input className="shadow appearance-none border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+                            <input className="shadow appearance-none border w-full py-2 px-3 text-gray-700 md:mb-3 leading-tight focus:outline-none focus:shadow-outline" />
                         </Form.Item>
                         <Form.Item
                             name="email"
@@ -77,7 +84,7 @@ const Signup = () => {
                                 </label>
                             }
                         >
-                            <input className="shadow appearance-none border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+                            <input className="shadow appearance-none border w-full py-2 px-3 text-gray-700 md:mb-3 leading-tight focus:outline-none focus:shadow-outline" />
                         </Form.Item>
                         <Form.Item
                             name="password"
@@ -94,7 +101,7 @@ const Signup = () => {
                                 </label>
                             }
                         >
-                            <Input.Password className="shadow appearance-none border w-full py-2 px-3 text-gray-900 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+                            <Input.Password className="shadow appearance-none border w-full py-2 px-3 text-gray-900 md:mb-3 leading-tight focus:outline-none focus:shadow-outline" />
                         </Form.Item>
                         <Form.Item
                             name="confirmPassword"
@@ -111,7 +118,7 @@ const Signup = () => {
                                 </label>
                             }
                         >
-                    <Input.Password className="shadow appearance-none border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+                    <Input.Password className="shadow appearance-none border w-full py-2 px-3 text-gray-700 md:mb-3 leading-tight focus:outline-none focus:shadow-outline" />
                         </Form.Item>
 
                         <Form.Item>
